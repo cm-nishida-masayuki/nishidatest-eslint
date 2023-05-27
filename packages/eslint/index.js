c803f006a884af8a174185c628f88265c202de37
cm-nishida-masayuki <nishida.masayuki@classmethod.jp>
2023-05-27 14:17:00 +0900

    各FWの eslint を追加

--- packages/eslint/index.js
+++ packages/eslint/index.js
@@ -1,3 +1,24 @@
 module.exports = {
   extends: ["prettier"],
+  plugins: ["import", "unused-imports"],
+  rules: {
+    "unused-imports/no-unused-imports": "error",
+    "import/order": [
+      "error",
+      {
+        alphabetize: {
+          order: "asc",
+        },
+      },
+    ],
+    "unused-imports/no-unused-vars": [
+      "warn",
+      {
+        vars: "all",
+        varsIgnorePattern: "^_",
+        args: "after-used",
+        argsIgnorePattern: "^_",
+      },
+    ],
+  },
 };
--- packages/eslint/react.js
+++ packages/eslint/react.js
@@ -1,6 +1,10 @@
 module.exports = {
-  extends: ["plugin:react/recommended", "./typescript.js"],
-  plugins: ["react"],
+  extends: [
+    "plugin:react/recommended",
+    "plugin:jsx-a11y/recommended",
+    "./typescript.js",
+  ],
+  plugins: ["react", "jsx-a11y"],
   settings: {
     react: {
       version: "detect",
@@ -9,5 +13,12 @@ module.exports = {
   rules: {
     "react/jsx-uses-react": "off",
     "react/react-in-jsx-scope": "off",
+    "react/self-closing-comp": [
+      "error",
+      {
+        component: true,
+        html: true,
+      },
+    ],
   },
 };
